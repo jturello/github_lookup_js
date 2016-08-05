4fea486d198ab42f10eb04aa9dcd902886e13724
James Turello <jturello@yahoo.com>
2016-08-05 10:17:36 -0700

    add basic working api request/response functionality

--- js/github-interface.js
+++ js/github-interface.js
@@ -1,14 +1,14 @@
-var apiKey = require('./../.env').apiKey;
+var Github = require('./../js/github.js').githubModule;
 
+$(document).ready(function(){
 
-$(document).ready(function() {
-  $('#weatherLocation').click(function() {
-    var city = $('#location').val();
-    $('#location').val("");
-    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey).then(function(response){
-      $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
-    }).fail(function(error) {
-      $('.showWeather').text(error.responseJSON.message);
-    });
+  $('#getGithub').click(function(){
+    var user_id = $('#user_id').val();
+    $('#user_id').val("");
+
+    var githubObj = new Github();
+    githubObj.getRepos(user_id);
+
+    $('.output').text("user_id = " + user_id + "!");
   });
 });
--- js/github.js
+++ js/github.js
@@ -1,5 +1,29 @@
 var apiKey = require('./../.env').apiKey;
 
+function Github(){
+}
+
+Github.prototype.getRepos = function(user_id) {
+  console.log("Hi! I'm a github object.");
+  console.log("This is the user_id: " + user_id);
+  $.get('https://api.github.com/users/' + user_id + '?access_token=' + apiKey).then(function(response){
+    console.log(response);
+  }).fail(function(error){
+    console.log(error.responseJSON.message);
+  });
+};
+
+exports.githubModule = Github;
+
+
+
+
+
+
+
+
+
+
 exports.getRepos = function(){
   $.get('https://api.github.com/users/daneden?access_token=' + apiKey).then(function(response){
     console.log(response);
