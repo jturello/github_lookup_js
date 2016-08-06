df507afe118cb5d0de1dcb63830ef4dc4c8015cd
James Turello <jturello@yahoo.com>
2016-08-05 17:59:42 -0700

    complete repo list display view and functionality

--- js/github-interface.js
+++ js/github-interface.js
@@ -1,17 +1,24 @@
 var Github = require('./../js/github.js').githubModule;
 
+var displayRepoInfo = function(user_id, repos) {
+  for (i=0; i < repos.length; i++) {
+    $('#repo_list').append("<li><a href='" + repos[i][0] + "'>" + repos[i][1] + " : " + repos[i][2] + "</a></li>");
+  }
+};
+
 $(document).ready(function(){
 
   $('#getGithub').click(function(){
     var user_id = $('#user_id').val();
     $('#user_id').val("");
 
-    if(user_id !== "") {
-      var githubObj = new Github();
-      var result = githubObj.getRepos(user_id);
-      
-      console.log("hi");
-    }
+    if(user_id === "") { return; }
+
+    var githubObj = new Github();
+    githubObj.getRepos(user_id, displayRepoInfo);
+
+    $('#repo_list_header').text("Public github repos for id - " + user_id + ":");
+    $('#repo_list_sub_header').text("Link format: 'project_name : project_description'. Click on a link to open the project page in github.")
 
   });
 });
--- js/github.js
+++ js/github.js
@@ -1,23 +1,26 @@
 var apiKey = require('./../.env').apiKey;
 
 function Github(){
-  var result = "";
 }
 
-Github.prototype.getRepos = function(user_id) {
+Github.prototype.getRepos = function(user_id, displayFunction) {
+  var repos = [];
+  var repo = [];
 
-  console.log("Hi! I'm a github object.");
-  console.log("This is the user_id: " + user_id);
+  $.get('https://api.github.com/users/' + user_id + '/repos?access_token=' + apiKey).then(function(response){
 
-  $.get('https://api.github.com/users/' + user_id + '?access_token=' + apiKey).then(function(response){
+    for (i=0; i < response.length; i++){
+      repo = [];
+      repo.push(response[i].owner.html_url + "/" + response[i].name);
+      repo.push(response[i].name);
+      repo.push(response[i].description);
 
-    console.log(JSON.stringify(response));
-    // console.log(JSON.stringify(response[0].name));
-    // console.log(JSON.stringify(response[0].description));
-    return response;
+      repos.push(repo);
+    }
+    displayFunction(user_id, repos);
 
   }).fail(function(error){
-    console.log(error.responseJSON.message);
+    $('.err').text(error.responseJSON.message);
   });
 };
 
